c9c7fefa85a7e8ee513a54edf9eec1cde027f20c
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-05-14 10:41:36 +0200

    fixed cobol installation?

--- ci/install_requirements.sh
+++ ci/install_requirements.sh
@@ -1,7 +1,3 @@
 set -e
-sudo apt-get update && \
-    sudo apt-get -y install autoconf build-essential && \
-    curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz && \
-    cd gnucobol-3.0-rc1 && ./configure && make && make install && ldconfig && cd /tmp/ && rm -rf * && \
-    sudo apt-get -y --purge autoremove autoconf build-essential && \
-    sudo apt-get -y clean 
+sudo apt-get update
+sudo apt-get install open-cobol
