2efc74c99365424c777981aaf43aea8652131c95
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-05-14 10:45:55 +0200

    added module tests

---
+++ project-cobol-unit-test/src/test/cobol/myPackage/MainUT.cbl
@@ -0,0 +1,22 @@
+           TESTSUITE 'GREETING AND FAREWELL (FAREWELL WILL FAIL)'
+
+           TESTCASE 'This works1'
+           MOVE 'Hans' TO WS-NAME
+           PERFORM 2000-COMPUTE-GREETING
+           EXPECT WS-GREETING-RESULT TO BE 'HELLO Hans'
+
+
+           TESTCASE 'This works2'
+           MOVE 'Hans' TO WS-NAME
+           PERFORM 2000-COMPUTE-GREETING
+           EXPECT WS-GREETING-RESULT TO BE 'HELLO Hans'
+
+           TESTCASE 'This works3'
+           MOVE 'Hans' TO WS-NAME
+           PERFORM 2000-COMPUTE-GREETING
+           EXPECT WS-GREETING-RESULT TO BE 'HELLO Hans'
+
+           TESTCASE 'This works4'
+           MOVE 'Hans' TO WS-NAME
+           PERFORM 2000-COMPUTE-GREETING
+           EXPECT WS-GREETING-RESULT TO BE 'HELLO Hans'
