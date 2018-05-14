a4d1de87d147c7284798bcf5b6cf99d501a5a95a
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-05-14 10:34:18 +0200

    added open cobol instalation

--- ci/install_requirements.sh
+++ ci/install_requirements.sh
@@ -1,2 +1,8 @@
 set -e
-sudo apt-get install open-cobol gradle
+apt-get update && \
+    apt-get -y install autoconf build-essential && \
+    curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz && \
+    cd gnucobol-3.0-rc1 && ./configure && make && make install && ldconfig && cd /tmp/ && rm -rf * && \
+    apt-get -y --purge autoremove autoconf build-essential && \
+    apt-get -y clean && \
+rm -rf /var/lib/apt/lists/*
