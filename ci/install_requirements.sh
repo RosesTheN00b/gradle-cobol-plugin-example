ae668676a718df8aff5a5b4abc9601065455a5d1
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-05-14 10:38:31 +0200

    added root permission on isntall dependencies

--- ci/install_requirements.sh
+++ ci/install_requirements.sh
@@ -1,8 +1,7 @@
 set -e
-apt-get update && \
-    apt-get -y install autoconf build-essential && \
+sudo apt-get update && \
+    sudo apt-get -y install autoconf build-essential && \
     curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz && \
     cd gnucobol-3.0-rc1 && ./configure && make && make install && ldconfig && cd /tmp/ && rm -rf * && \
-    apt-get -y --purge autoremove autoconf build-essential && \
-    apt-get -y clean && \
-rm -rf /var/lib/apt/lists/*
+    sudo apt-get -y --purge autoremove autoconf build-essential && \
+    sudo apt-get -y clean 
