9d6c67dd460c848d3ec2ab5f59edb02acbc03f1d
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-06-08 12:03:03 +0200

    added build ouput

--- ci/installGnuCobol.sh
+++ ci/installGnuCobol.sh
@@ -1,4 +1,11 @@
 
-apt-get -y install autoconf build-essential && \
-    curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz && \
-    cd gnucobol-3.0-rc1 && ./configure && make && make install && ldconfig && cd ..
+set -e
+echo "start installing gnu cobol 3.0"
+apt-get install autoconf build-essential
+curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz && 
+cd gnucobol-3.0-rc1 
+./configure 
+make 
+make install 
+ldconfig 
+cd ..
