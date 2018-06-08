8d6803052c1bfd4cb928693fc2a61d0d6839b792
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-06-08 11:37:42 +0200

    updated cut dirs of wget§

--- ci/installGnuCobol.sh
+++ ci/installGnuCobol.sh
@@ -1,6 +1,6 @@
 set -e
 
-wget -N -e robots=off -r -np -nH --cut-dirs =5  http://svn.code.sf.net/p/open-cobol/code/branches/gnucobol-2.x
+wget -N -e robots=off -r -np -nH --cut-dirs 5  http://svn.code.sf.net/p/open-cobol/code/branches/gnucobol-2.x
 
 tar xvf gnucobol-2.x.tar.gz
 cd gnucobol-2.x
