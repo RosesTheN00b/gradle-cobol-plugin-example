1258e03e7f66c5039332f4d98a780ffee2feee2a
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-06-11 12:47:18 +0200

    refactored cbl files

---
+++ project-cobol-unit-test/src/main/cobol/coveredPackage/MainInPackage.cbl
@@ -0,0 +1,22 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FullTested.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01  WS-NAME PIC X(4).
+       01  WS-SALUTE PIC X(10).
+       01  WS-GREETING PIC X(6) VALUE 'HELLO '.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+
+       2000-COMPUTE-GREETING.
+       *> This file is 100% tested
+           STRING WS-GREETING WS-NAME INTO WS-SALUTE END-STRING.
+           DISPLAY WS-GREETING-RESULT.
+       END PROGRAM FullTested.
