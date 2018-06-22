66218ba6cf9a19bbbf5cb6ca8653178d04750a93
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-06-22 11:08:45 +0200

    added berkley db

--- ci/install_requirements.sh
+++ ci/install_requirements.sh
@@ -1,10 +1,21 @@
 set -e
+
 sudo apt-get update
 sudo apt-get -y install autoconf build-essential
+
+curl -sLk https://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz | tar xz
+cd db-4.8.30.NC/build_unix
+../dist/configure --enable-cxx --prefix=/usr 
+make install
+make clean 
+cd ..
+
+
+
 curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz
 cd gnucobol-3.0-rc1 
 ./configure 
 make
 sudo make install 
 ldconfig
-
+cd ..
