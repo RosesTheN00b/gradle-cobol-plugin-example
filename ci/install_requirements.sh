4ae662d5546dfdd1cf972af0ff762c2204d30e04
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-06-22 11:03:18 +0200

    switrched to gnucobol 3.0

--- ci/install_requirements.sh
+++ ci/install_requirements.sh
@@ -1,4 +1,10 @@
 set -e
 sudo apt-get update
-wget http://mirrors.kernel.org/ubuntu/pool/universe/g/gnucobol/gnucobol_2.2-2_amd64.deb
-sudo apt install ./gnucobol_2.2-2_amd64.deb
+sudo apt-get -y install autoconf build-essential
+curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-3.0-rc1.tar.gz | tar xz \
+cd gnucobol-3.0-rc1 
+./configure 
+make
+sudo make install 
+ldconfig
+
