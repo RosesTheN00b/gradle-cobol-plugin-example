c8d9e50dc5912d47dd2589b1fa49199d057eda3d
RosesTheN00b <git@sebastianruziczka.de>
2019-02-11 21:23:11 +0100

    added ci builds dev

---
+++ ci/run_core_dev_test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -e
+sh ci/pull_plugin_sources.sh
+
+cd gradle-cobol-plugin/gradle-cobol-plugin-core
+git checkout dev
+cd ../..
+
+sh ci/build_plugin.sh
+sh ci/install_plugin.sh
+sh ci/exec_tests.sh
