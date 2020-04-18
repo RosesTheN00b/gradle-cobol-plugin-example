52cbda0ee1c1e4e8ae4d6008c2d2a281e1afd698
RosesTheN00b <RosesTheN00b@github>
2020-04-18 19:05:03 +0200

    switch to gradle wrapper to meet new gradle plugin version dependencies

--- ci/exec_tests.sh
+++ ci/exec_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd project-cobol-fixed-format && gradle check && cd ..
-cd project-cobol-free-format && gradle check && cd ..
-cd project-cobol-unit-test && gradle check && cd ..
-cd project-cobol-unit-test && gradle testUnit computeTestCoverage && cd ..
+cd project-cobol-fixed-format && ./gradlew check && cd ..
+cd project-cobol-free-format && ./gradlew check && cd ..
+cd project-cobol-unit-test && ./gradlew check && cd ..
+cd project-cobol-unit-test && ./gradlew testUnit computeTestCoverage && cd ..
 cat project-cobol-unit-test/build/CobolUnit/coverage.xml
-cd project-cobol-integration-test && gradle check && cd ..
-cd project-cobol-integration-test && gradle testIntegration computeIntegrationTestCoverage --debug --stacktrace && cd ..
+cd project-cobol-integration-test && ./gradlew check && cd ..
+cd project-cobol-integration-test && ./gradlew testIntegration computeIntegrationTestCoverage --debug --stacktrace && cd ..
 cat project-cobol-integration-test/build/CobolUnit/integration-coverage.xml
